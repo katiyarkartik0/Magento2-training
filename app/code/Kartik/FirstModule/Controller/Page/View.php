7373dc00c0bf930e115877acd2ae3391585901f5
Kartik Katiyar <kartik.katiyar@hbwsl.com>
2023-10-26 14:41:30 +0530

    created repository

---
+++ app/code/Kartik/FirstModule/Controller/Page/View.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kartik\FirstModule\Controller\Page;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\Result\Json;
+use Magento\Framework\Controller\ResultFactory;
+
+class View extends Action
+{ 
+    public function execute()
+    {
+        $jsonResult = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+        $jsonResult->setData(['message'=>'My first Page','newMessage'=>'My second Pae']);
+        return $jsonResult;
+    }
+}
