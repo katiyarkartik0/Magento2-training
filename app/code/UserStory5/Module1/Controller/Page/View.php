9d7d505d6e6655a2e2c13e37c5d3f86bb0bfe728
Kartik Katiyar <katiyarkartik0@gmail.com>
2023-11-07 17:12:12 +0530

    completed userStory3 part 4

--- app/code/UserStory5/Module1/Controller/Page/View.php
+++ app/code/UserStory5/Module1/Controller/Page/View.php
@@ -3,9 +3,27 @@
 namespace UserStory5\Module1\Controller\Page;
 
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\ResultFactory;
 
-class View extends Action{
-    public function execute(){
+class View extends Action
+{
+    protected $resultFactory;
+
+    public function __construct(
+        ResultFactory $resultFactory,
+        Context $context
+    ) {
+        $this->resultFactory = $resultFactory;
+
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
         echo "Hello World";
+        $redirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        $redirect->setUrl('/tupac-print-hoodie.html');
+        return $redirect;
     }
 }
