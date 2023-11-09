30997c692b71b599c9aa1b69b3ef1875d8d2e7f4
Kartik Katiyar <katiyarkartik0@gmail.com>
2023-11-09 10:19:34 +0530

    spelling updated

--- app/code/UserStory7/Module1/Block/Message.php
+++ app/code/UserStory7/Module1/Block/Message.php
@@ -4,7 +4,7 @@ namespace UserStory7\Module1\Block;
 
 use Magento\Framework\View\Element\Template;
 
-class message extends Template{
+class Message extends Template{
     protected function _toHtml()
     {
         return '<div>UserStory#7Module1 My Message</div>';
