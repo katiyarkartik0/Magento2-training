edaa3a7d12b9488567dd6eef3f197f5a5c14086c
Kartik Katiyar <katiyarkartik0@gmail.com>
2023-11-20 15:11:56 +0530

    userstory#1 deleted

--- app/code/Kartik/FirstModule/registration.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    'Kartik_FirstModule',__DIR__
-);
--- app/code/Kartik/Test/registration.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    'Kartik_Test',__DIR__
-);
