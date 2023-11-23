29e97aae9cb5eefc23b7873922c42d92972ad7a1
Kartik Katiyar <katiyarkartik0@gmail.com>
2023-11-23 12:50:37 +0530

    completed video22

---
+++ app/code/Video22/DependencyInjection/Model/Injectable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Video22\DependencyInjection\Model;
+
+use Video22\DependencyInjection\Model\InjectableInterface;
+
+class Injectable implements InjectableInterface
+{
+    public function getId(): string
+    {
+        return 'Class Injectable';
+    }
+}
