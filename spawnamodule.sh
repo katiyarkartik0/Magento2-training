7e75749ba2b63f960bd1d296f978ba22c01f5c8e
Kartik Katiyar <katiyarkartik0@gmail.com>
2023-11-24 17:13:05 +0530

    spawnamodule updated

--- spawnamodule.sh
+++ spawnamodule.sh
@@ -6,11 +6,21 @@ if [ "$#" -ne 2 ]; then
   exit 1
 fi
 
+########################################################################################################
+#VARIABLES
 vendor_name=$1
 module_name=$2
+route_id=$(echo "${vendor_name}${module_name}" | tr '[:upper:]' '[:lower:]')
+vendorName_moduleName="${vendor_name}_${module_name}"
+
+#########################################################################################################
+
+
 
 # Magento 2 app/code directory
 magento_code_dir=$(pwd)/"app/code"
+
+
 
 # Check if the vendor directory already exists
 if [ -d "$magento_code_dir/$vendor_name" ]; then
@@ -20,6 +30,99 @@ fi
 
 # Create the directory structure
 module_dir="$magento_code_dir/$vendor_name/$module_name"
+
+###########################################################################################################################################################
+#FUNCTIONS
+create_modulexml(){
+  # Create etc/module.xml
+cat > "$module_dir/etc/module.xml" <<EOL
+<?xml version="1.0"?>
+<config xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="urn:magento:framework:Module/etc/module.xsd">
+    <module name="${vendorName_moduleName}" setup_version="1.0.0">
+        <!-- Add any module-specific configuration here -->
+    </module>
+</config>
+EOL
+}
+
+create_registrationphp(){
+  # Create registration.php
+cat > "$module_dir/registration.php" <<EOL
+<?php
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    "${vendorName_moduleName}",
+    __DIR__
+);
+EOL
+}
+create_indexphp(){
+  #Create Index.php
+  cat > "$module_dir/Controller/Index/Index.php" <<EOL
+<?php
+
+namespace ${vendor_name}\\${module_name}\\Controller\\Index;
+
+use Magento\\Framework\\App\\ActionInterface;
+use Magento\\Framework\\View\\Result\\PageFactory;
+
+class Index implements ActionInterface {
+    protected \$pageFactory;
+
+    public function __construct(PageFactory \$pageFactory) {
+        \$this->pageFactory = \$pageFactory;
+    }
+
+    public function execute() {
+        \$resultPage = \$this->pageFactory->create();
+        return \$resultPage;
+    }
+}
+EOL
+}
+
+create_routesxml(){
+  #Create routes.xml
+    cat > "$module_dir/etc/frontend/routes.xml" <<EOL
+<?xml version="1.0"?>
+<config xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="urn:magento:framework:App/etc/routes.xsd">
+    <router id="standard">
+        <route id="$route_id" frontName="$route_id">
+            <module name="${vendorName_moduleName}" />
+        </route>
+    </router>
+</config>
+EOL
+}
+
+create_routeIdindexindexxml(){
+  #Create routeId_index_index.xml
+  cat > "$module_dir/view/frontend/layout/${route_id}_index_index.xml" <<EOL
+<?xml version="1.0"?>
+<page xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="urn:magento:framework:View/Layout/etc/page_configuration.xsd">
+    <referenceContainer name="content">
+        <block 
+            class="Magento\Framework\View\Element\Template" 
+            name="blockname" 
+            template="${vendorName_moduleName}::templateName.phtml" 
+        />
+    </referenceContainer>
+</page>
+EOL
+}
+
+create_templateNamephtml(){
+  #create templateName.phtml
+    cat > "$module_dir/view/frontend/templates/templateName.phtml" <<EOL
+<div>
+    <h2>Welcome to ${vendorName_moduleName}</h2>
+</div>
+EOL
+}
+
+###########################################################################################################################################################
 
 echo "Creating directory structure for $vendor_name/$module_name..."
 
@@ -35,53 +138,41 @@ mkdir -p "$module_dir"
 mkdir "$module_dir/etc"
 
 # Create etc/module.xml
-cat > "$module_dir/etc/module.xml" <<EOL
-<?xml version="1.0"?>
-<config xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="urn:magento:framework:Module/etc/module.xsd">
-    <module name="${vendor_name}_${module_name}" setup_version="1.0.0">
-        <!-- Add any module-specific configuration here -->
-    </module>
-</config>
-EOL
+create_modulexml
 
 # Create registration.php
-cat > "$module_dir/registration.php" <<EOL
-<?php
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    "${vendor_name}_${module_name}",
-    __DIR__
-);
-EOL
+create_registrationphp
 
 # Function to create folders based on user input
 create_folders_Blck_Cntr_Obs_Plg() {
   #vendorname_modulename in lowercase are used as route_id
-  route_id=$(echo "${vendor_name}${module_name}" | tr '[:upper:]' '[:lower:]')
 
   mkdir "$module_dir/Block"
-  mkdir "$module_dir/Controller"
+  mkdir -p "$module_dir/Controller/Index"
+
+  #Create Index.php
+  create_indexphp
+
   mkdir "$module_dir/Observer"
   mkdir "$module_dir/Plugin"
   
   mkdir -p "$module_dir/etc/frontend"
-  cat > "$module_dir/etc/frontend/routes.xml" <<EOL
-<?xml version="1.0"?>
-<config xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="urn:magento:framework:App/etc/routes.xsd">
-    <router id="standard">
-        <route id="$route_id" frontName="$route_id">
-            <module name="${vendor_name}_${module_name}" />
-        </route>
-    </router>
-</config>
-EOL
+
+  #Create routes.xml
+  create_routesxml;
 }
 
 create_folders_view(){
   mkdir -p "$module_dir/view/frontend/layout"
+
+  #Create routeId_index_index.xml
+  create_routeIdindexindexxml
+
   mkdir -p "$module_dir/view/frontend/templates"
+
+  #Create templateNamephtml
+  create_templateNamephtml
+
   mkdir -p "$module_dir/view/frontend/web/css"
   mkdir -p "$module_dir/view/frontend/web/js"
   mkdir -p "$module_dir/view/frontend/web/template"
