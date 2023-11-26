e7a44503e9ba42b8bc2af3aecb39af8cbf411d4b
Kartik Katiyar <katiyarkartik0@gmail.com>
2023-11-26 13:30:34 +0530

    video11

---
+++ app/code/Video11/WidgetExample/Block/Widget/Title.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Video11\WidgetExample\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class Title extends Template implements BlockInterface{
+    protected $_template = 'widget/title.phtml';
+}
